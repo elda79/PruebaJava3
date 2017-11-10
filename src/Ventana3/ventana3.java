82370c1f026d3c60ff1a2e2e3876674d983c7c4e
negrita79 <elisa.79@live.cl>
2017-11-10 19:47:07 -0300

    encabezado ventana3 realizado

--- src/Ventana3/ventana3.java
+++ src/Ventana3/ventana3.java
@@ -9,8 +9,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 /**
- *
+ *Elda Solar
  * @author LAB05
+ * Fecha: 10/11/2017
+ * Clase:PruebaJava3
+ * Responsabilidad:conexion con MySQL,declara los objetos para la conexion,metodo para manejar excepciones y Define metodos para conectar y desconectar
  */
 public class ventana3 {
     
