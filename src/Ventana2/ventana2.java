7e3b195e238f463b7dfe42199fda8fe8bad1b8b6
negrita79 <elisa.79@live.cl>
2017-11-10 19:34:23 -0300

    realizado encabezado ventana2

--- src/Ventana2/ventana2.java
+++ src/Ventana2/ventana2.java
@@ -7,9 +7,12 @@ package Ventana2;
 // Paso 8 - IMPORTAR LA CLASE CON LA VENTANA
 import Ventana1.ventana1;//import paquete.clase
 import javax.swing.JFrame;
-/**
- *
+/******************************************************************************
+ *Elda Solar
  * @author LAB05
+ * Fecha: 10/11/2017
+ * Clase:PruebaJava3
+ * Responsabilidad:ventana 2 se importa la clase con la ventana y se hace llamado a la clase JFrame
  */
 public class ventana2 {
     
