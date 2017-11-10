2bc8fd2088b36b6edcc5af48a04c01a55153acd1
negrita79 <elisa.79@live.cl>
2017-11-10 19:06:51 -0300

    Paso 1 - IMPORTAMOS LAS LIBRERIAS

--- src/Ventana1/ventana1.java
+++ src/Ventana1/ventana1.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package Ventana1;
-
+//Paso 1 - IMPORTAMOS LAS LIBRERIAS
+import javax.swing.*;//Importamos todo de swing
+import java.awt.*;
 /**
  *
  * @author LAB05
