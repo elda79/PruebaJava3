39b2a6f8fdbb0f02611aa14c7eb7298c7f0d60c3
negrita79 <elisa.79@live.cl>
2017-11-10 19:40:33 -0300

    Paso 9 - Llamado a la clase JFrame

--- src/Ventana2/ventana2.java
+++ src/Ventana2/ventana2.java
@@ -16,4 +16,20 @@ import javax.swing.JFrame;
  */
 public class ventana2 {
     
+
+     public static void main(String[] args) 
+{
+        // TODO code application logic here
+        // Paso 9 - Llamado a la clase JFrame
+        ventana1 ventana = new ventana1(); // Creo un objeto tipo Ventana1
+        ventana.setVisible(true);
+        ventana.setBounds(0, 0, 600, 800);
+        //La siguiente inst. le ppone titulo a la ventana
+        ventana.setTitle("Tercera Evaluacion Sumativa");
+        
+         //La siguiente sentencia es para finalizar el proyecto al cerrar
+         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         
+             
+ }
 }
